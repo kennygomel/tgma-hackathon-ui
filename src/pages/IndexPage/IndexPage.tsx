ab454e163c9b65a3ca4cc0a7348f2819ad78b790
Ignat Rychkov <ignatrychkov@gmail.com>
2025-09-18 17:46:34 +0300

    Small updates

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,28 +1,51 @@
+import type { DisplayDataRow } from '@/components/DisplayData/DisplayData.tsx';
+import {
+  initDataState as _initDataState,
+  type User,
+  useSignal,
+} from '@telegram-apps/sdk-react';
 import { Section, Cell, Image, List, Tabbar } from '@telegram-apps/telegram-ui';
 import { Icon24ChevronDown } from '@telegram-apps/telegram-ui/dist/icons/24/chevron_down';
 import { Icon24ChevronLeft } from '@telegram-apps/telegram-ui/dist/icons/24/chevron_left';
 import { Icon24ChevronRight } from '@telegram-apps/telegram-ui/dist/icons/24/chevron_right';
-import type { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 import { Link } from '@/components/Link/Link.tsx';
 import { Page } from '@/components/Page.tsx';
 
 import tonSvg from './ton.svg';
 
+function getUserRows(user: User): DisplayDataRow[] {
+  return Object.entries(user).map(([title, value]) => ({ title, value }));
+}
+
 export const IndexPage: FC = () => {
+  const initDataState = useSignal(_initDataState);
+
+  const userRows = useMemo<DisplayDataRow[] | undefined>(() => {
+    return initDataState && initDataState.user
+      ? getUserRows(initDataState.user)
+      : undefined;
+  }, [initDataState]);
+
+  const fullName = userRows
+    ?.filter(row => ['first_name', 'last_name'].includes(row.title))
+    .map(row => row.value)
+    .filter(Boolean)
+    .join(' ') as string;
+  const photoUrl = userRows?.find(row => row.title === 'photo_url')?.value as string;
+  const username = userRows?.find(row => row.title === 'username')?.value as string;
+
   return (
     <Page back={false}>
       <List>
-        <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
-        >
+        <Section header="Мой аккаунт">
           <Link to="/ton-connect">
             <Cell
-              before={<Image src={tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
-              subtitle="Connect your TON wallet"
+              before={<Image src={photoUrl || tonSvg} style={{ backgroundColor: '#007AFF' }}/>}
+              subtitle={username ? `@${username}` : null}
             >
-              TON Connect
+              {fullName}
             </Cell>
           </Link>
         </Section>
