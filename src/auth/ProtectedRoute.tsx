4479dd0a64cf53f02c30e6b1be93765ab6e62382
Ignat Rychkov <ignatrychkov@gmail.com>
2025-09-20 01:56:44 +0300

    General updates

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -3,11 +3,11 @@ import { useAuth } from "./AuthContext";
 
 // export default function ProtectedRoute({ role }: { role?: string }) {
 export default function ProtectedRoute() {
-  const { isAuthorized, loading } = useAuth();
+  const { user, loading } = useAuth();
 
   if (loading) return null; // спиннер по вкусу
 
-  if (!isAuthorized) return <Navigate to="/" replace />;
+  if (!user) return <Navigate to="/" replace />;
 
   // if (role && !user.roles.includes(role)) return <Navigate to="/403" replace />;
 
