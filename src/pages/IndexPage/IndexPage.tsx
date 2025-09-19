82e281db4da3a47409b0c539661b3335d7ebedeb
Ignat Rychkov <ignatrychkov@gmail.com>
2025-09-19 22:25:49 +0300

    General updates

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -20,7 +20,7 @@ function getUserRows(user: User): DisplayDataRow[] {
 }
 
 export const IndexPage: FC = () => {
-  const initDataRaw = import.meta.env.DEV ? 'user=%7B%22id%22%3A323173181%2C%22first_name%22%3A%22Ignat%22%2C%22last_name%22%3A%22Rychkov%22%2C%22username%22%3A%22ignatrychkov%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2Ff6LJeyec9XyXOSr_OXRWo3HKqbKFlfCYYkM01fOQalI.svg%22%7D&chat_instance=1432267138820610369&chat_type=private&auth_date=1758284032&signature=aC41ox5qarx_tQECxPQ_kO9Wjy_9NlZALi6pBQqaPEmqah6uMHuTw45HUQGccutAOVN_flii26CNwq72h2y7AQ&hash=a3a859a76ffa75f91abd7fc75234f0b81cfaa6c5e0c4ba1de13de34b798792ec' : useSignal(_initDataRaw);
+  const initDataRaw = useSignal(_initDataRaw);
   const initDataState = useSignal(_initDataState);
   const { data: user, loading, error } = useQuery<AuthUser>(() => AuthService.signIn(initDataRaw as string), []);
 
